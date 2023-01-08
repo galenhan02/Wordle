58048ab8b1a75a848ed06a7cb3bb2b0686575c33
Galen <galen.han02@gmail.com>
2023-01-07 20:59:49 -0800

    Initial Commit

---
+++ Wordle/WordleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordleApp.swift
+//  Wordle
+//
+//  Created by Galen Han on 1/7/23.
+//
+
+import SwiftUI
+
+@main
+struct WordleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
